c5492ebc9e517cd1204229c13051685e66226390
itx-mj65 <jamshedweb65@gmail.com>
2025-08-25 11:36:08 +0500

    25 of august

---
+++ 22-Aug/main.js
@@ -0,0 +1,30 @@
+let date = 2;
+
+// console.log(date)
+
+
+switch (date) {
+    case 0:
+        console.log("sunday")
+        break;
+    case 1:
+        console.log("monday")
+        break;
+    case 2:
+        console.log("tuesday")
+        break;
+    case 3:
+        console.log("wednesday")
+        break;
+    case 2:
+        console.log("tuesday in second")
+        break;
+    case 5:
+        console.log("friday")
+        break;
+    case 6:
+        console.log("saturday")
+        break;
+    default:
+        console.log("Invalid day")
+}
---
+++ 25-Aug/main.js
@@ -0,0 +1,22 @@
+
+
+function hello(num1, num2) {
+    let sum = num1 + num2
+
+    return  "this is return " + sum;
+}   // declaration of function
+
+// function hello1() {
+//     let b = 30;
+//     let c = hello(20, 30)
+//     console.log(b + c)
+// }
+
+
+
+console.log(hello(200, 500))
+
+// function call or invoke
+// console.log(hello(120, 30))
+// console.log(hello(120, 349))
+
