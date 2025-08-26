8ba6fd43a868465aa78cd0c2601c52ddb0f997b9
itx-mj65 <jamshedweb65@gmail.com>
2025-08-26 11:07:36 +0500

    26 of august

---
+++ 26-Aug/main.js
@@ -0,0 +1,20 @@
+
+let b = !(a == 30)
+console.log(b)
+
+
+
+if (!(a > 30)) {
+    console.log("the code is executed with at least one statement true  true ")
+}
+
+let a = 20
+
+for (let x = 0; x <= 10; x++) {
+    document.write(x * a , "<br>")
+}
+ function hello(a, b , c ){
+
+ }
+
+ 
