ba1f457aa0812e1bcfe0aeec5f8b2ebf89085985
itx-mj65 <jamshedweb65@gmail.com>
2025-08-27 10:59:21 +0500

    27 of august

---
+++ 27-Aug/main.js
@@ -0,0 +1,30 @@
+// for (let a = 0; a < 10; a++) {
+//     console.log("hello world", a)
+// }
+
+let a = 11;
+while (a < 10) {
+    console.log("hello world", a)
+    a++;
+
+}
+
+// let b = 11
+// do {
+//     console.log("hello world with b ", b)
+//     b++
+
+// } while (b < 10)
+
+
+let arr = ["hello", "world", "web", 647, false]
+
+// console.log(arr.length)
+// console.log(arr.toString())
+// console.log(arr[1])
+// console.log(arr.at(1))
+// console.log(arr.join(" $ hello "))
+// arr.pop()
+// arr.push("new value")
+arr.unshift("new value")
+console.log(arr)
