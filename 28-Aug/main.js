8e6b1955d072a323ad9b7c39f8a5eb666e448307
itx-mj65 <jamshedweb65@gmail.com>
2025-08-28 11:15:09 +0500

    28 of august

---
+++ 28-Aug/main.js
@@ -0,0 +1,19 @@
+
+// let arr = [[1, 2], [3, 4, 5]]
+let arr2 = [6, 7, "world", 9, 10]
+
+
+// delete arr[2]
+
+// console.log(arr)
+// arr.pop()
+// console.log(arr)
+
+// console.log(arr.flat())
+// console.log(arr.concat(arr2))
+
+console.log(arr2.slice(1, 2))
+
+
+console.log(arr2.splice(2, 1))
+console.log(arr2)
