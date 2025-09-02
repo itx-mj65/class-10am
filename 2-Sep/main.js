53384af9c5488d068c4d30c2478da61866b6644e
itx-mj65 <jamshedweb65@gmail.com>
2025-09-02 11:33:15 +0500

    2 of sep

---
+++ 2-Sep/main.js
@@ -0,0 +1,30 @@
+
+// let arr = [2, 4, 6, 8, 3, 4]
+// let result = arr.map((koch) => {
+//     return koch * 2
+// })
+
+// result.map((item) => {
+//     console.log(item * 4)
+// })
+// console.log(result)
+
+// let result2 = arr.forEach((koch) => {
+//     console.log(koch * 2)
+// })
+// result2.map((item) => {
+//     console.log(item * 4)
+// })
+
+
+// console.log(result2)
+// let result2 = arr.forEach((item) => {
+//     console.log( item * 2)
+
+// })
+
+// console.log(result2)
+
+
+
+console.log(window.document.childNodes[1].childNodes[2].childNodes)
