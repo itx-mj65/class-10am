94ac547c001bb31122c88be3582ceb5a63dbdaf7
itx-mj65 <jamshedweb65@gmail.com>
2025-09-04 11:12:05 +0500

    4 of september

---
+++ 4-Sep/main.js
@@ -0,0 +1,17 @@
+let main = document.getElementById("main");
+let newelement = document.createElement("h1");
+newelement.innerText = "hello world";
+document.getElementById("main").appendChild(newelement);
+
+let remove =document.getElementById("remove")
+// main.removeChild(remove)
+
+main.remove()
+
+// main.classList.add("bgcolor")
+// // main.classList.remove("added")
+// main.classList.toggle("toggleclass")
+
+// main.style.backgroundColor="yellow"
+// main.style.color="red"
+// main.style.borderRadius="10px"
